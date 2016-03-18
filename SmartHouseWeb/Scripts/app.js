4700c2d3efaee1782364664ad34a0043d8afaf9c
Andrew Obukhov <andrewobukhov@gmail.com>
2016-03-18 16:45:59 +0300

    Add bath_house and boiler_room

--- SmartHouseWeb/Scripts/app.js
+++ SmartHouseWeb/Scripts/app.js
@@ -3,18 +3,4 @@
 
     angular.module('app', ['underscore', 'ui.router']);
 
-    angular.module('app').config(function ($stateProvider, $urlRouterProvider) {
-
-        $urlRouterProvider.otherwise("/state1");
-
-        $stateProvider
-        .state('state1', {
-            url: "/state1",
-            templateUrl: "templates/state1.html"
-        })
-        .state('state2', {
-            url: "/state2",
-            templateUrl: "templates/state2.html"
-        });
-    });
 })();
